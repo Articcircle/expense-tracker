2ae7fae6010abf305db1584ccdaaf0ba68101c04
Sumit Thite <krishna.thite65@gmail.com>
2026-01-29 23:24:07 +0530

    Fix production settings

--- expense_tracker/settings.py
+++ expense_tracker/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v3*^g&jdh(go%r9670t^)^7tdjwag_1ap_06^s#d(m2l=lx)82"
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ['.onrender.com']
 
